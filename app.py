1450b9a6257ce2a6e2884f76ef8304cc5ec97339
koyo.takaya <koyo7jp@gmail.com>
2022-03-26 14:01:04 +0900

    line connection test

--- app.py
+++ app.py
@@ -1,45 +1,46 @@
+from distutils.command.config import config
 from flask import Flask, request, abort
 
 from linebot import (LineBotApi, WebhookHandler)
 from linebot.exceptions import (InvalidSignatureError)
 from linebot.models import (MessageEvent, TextMessage, TextSendMessage)
 
-# from config import *
+# from .flaskr.config import *
 
 app = Flask(__name__)
 
-# line_bot_api = LineBotApi(LINE_BOT_CHANNEL_ACCESS_TOKEN)
-# handler = WebhookHandler(LINE_BOT_CHANNEL_SECRET)
+line_bot_api = LineBotApi(LINE_BOT_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(LINE_BOT_CHANNEL_SECRET)
 
 
-# @app.route("/callback", methods=['POST'])
-# def callback():
-#     # get X-Line-Signature header value
-#     signature = request.headers['X-Line-Signature']
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
 
-#     # get request body as text
-#     body = request.get_data(as_text=True)
-#     app.logger.info("Request body: " + body)
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
 
-#     # handle webhook body
-#     try:
-#         handler.handle(body, signature)
-#     except InvalidSignatureError:
-#         print("Invalid signature. Please check your channel access token/channel secret.")
-#         abort(400)
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        print("Invalid signature. Please check your channel access token/channel secret.")
+        abort(400)
 
-#     return 'OK'
+    return 'OK'
 
 @app.route("/hi")
 def hello():
     return 'hi'
 
 
-# @handler.add(MessageEvent, message=TextMessage)
-# def handle_message(event):
-#     line_bot_api.reply_message(
-#         event.reply_token,
-#         TextSendMessage(text='im back chick'))
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text='im back chick'))
 
 
 if __name__ == "__main__":
