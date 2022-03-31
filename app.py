f8353052366c41323fad2625eb3b654b06692cfe
koyo.takaya <koyo7jp@gmail.com>
2022-03-31 20:14:20 +0900

    a

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ def handle_message(event):
         print(event.source)
 
         work_time_repository = WorkTimeRepository(
-            event["source"]["user_id"],
+            event.source.user_id,
             message_service.start_time,
             message_service.end_time
             )
