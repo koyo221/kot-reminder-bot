87733b94bcf57f9faa6ed6042689bae6c863319c
koyo.takaya <koyo7jp@gmail.com>
2022-03-31 20:07:12 +0900

    fixed?

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ def handle_message(event):
         print(event.source)
 
         work_time_repository = WorkTimeRepository(
-            event.source.user,
+            event.source["userId"],
             message_service.start_time,
             message_service.end_time
             )
